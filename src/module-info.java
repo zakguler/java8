e2eb0c45b9e640533dfb3da138f2f97de0584c1f
zakgu <zakgu@DESKTOP-KCIIRA4>
2019-07-06 15:56:31 -0600

    add module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,22 +1,27 @@
 module java_examples {
-	exports com.java.streams.noneMatch;
-	exports com.java.common_DATA;
-	exports com.java.test;
-	exports com.java.streams.intStream;
 	exports com.java.collections.mapList;
 	exports com.java.optionals;
 	exports com.java.streams.collector_2;
 	exports com.java.streams.LazyStream;
 	exports com.java.streams.collector;
+	exports com.java.streams.streambuilder;
+	exports com.java.lambda_functions.consumer;
+	exports com.java.streams.mapToObj;
+	exports com.java.lambda_functions.supplier;
+	exports com.java.streams.printHashMap;
+	exports com.java.test2;
+	exports com.java.Common_MODELS;
+	exports com.java.streams.noneMatch;
+	exports com.java.common_DATA;
+	exports com.java.test;
+	exports com.java.streams.intStream;
+	exports com.java.lambda_functions.method_reference;
 	exports com.java.streams.general;
 	exports com.java.array.arrays_asList;
 	exports com.java.streams.functional_programming.functional_interface;
 	exports com.java.interface_defaultMethod;
-	exports com.java.method_reference;
-	exports com.java.streams.mapToObj;
+	exports com.java.lambda_functions.custom_functionalInterface;
+	exports com.java.lambda_functions.predicate;
 	exports com.java.streams;
-	exports com.java.streams.printHashMap;
 	exports com.java.Common_ENUM;
-	exports com.java.test2;
-	exports com.java.Common_MODELS;
 }
